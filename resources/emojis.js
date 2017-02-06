66c629abe94cd49b403e3a12912b09621780c107
JP Wesselink <jpwesselink@gmail.com>
2017-02-06 12:28:16 +0100

    Create emojis.js

---
+++ resources/emojis.js
@@ -0,0 +1,10 @@
+[{
+  "name" : "NodeSchool specific"
+},{
+  "url": "https://cloud.githubusercontent.com/assets/1814479/22645494/964d489c-ec67-11e6-9f4f-9e8f62121249.png",
+  "labels": "slack",
+  "source": "Slack",
+  "license": "?",
+  "size": "84x84",
+  "name": "slack"
+}]
